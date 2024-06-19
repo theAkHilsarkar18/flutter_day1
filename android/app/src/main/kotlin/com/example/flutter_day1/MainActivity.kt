89a159d76552510781dbe8112be829d06b939cf2
RW <prafulpatnecha@gmail.com>
2024-06-19 11:49:59 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_day1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_day1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Day 4'),
+        ),
+        body: Center(
+          child: Text('Hello Flutter !'),
+        ),
+      ),
+    ),
+  );
+}
+
+// Github : Version Control System
+
+// version control -> share project on github ->
